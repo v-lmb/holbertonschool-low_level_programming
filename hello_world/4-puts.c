b76e62e3d007f4547cb9d66f3cbe9f961b7c57b3
v-lmb <virginie.amamelia@gmail.com>
2025-10-20 14:40:23 +0000

    feat everything to please betty

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - petite description
+ * Return: petit return
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
