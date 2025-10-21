047c6e43298f1c4b6e513a3ff470c8a5b04e2a3a
v-lmb <virginie.amamelia@gmail.com>
2025-10-21 15:39:52 +0000

    fix correct the sentence in printf, replacing is and and in the right place

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ int main(void)
 	D = n % 10;
 	if (n > D)
 	{
-		printf("Last digit of %d and %d is greater than 5\n", n, D);
+		printf("Last digit of %d is %d and is greater than 5\n", n, D);
 	}
 	else if (n < D)
 	{
-		printf("Last digit of %d and %d is less than 6 and not 0\n", n, D);
+		printf("Last digit of %d is %d ans is less than 6 and not 0\n", n, D);
 	}
 	else
 	{
-		printf("Last digit of %d and %d is 0\n", n, D);
+		printf("Last digit of %d is %d and is 0\n", n, D);
 	}
 return (0);
 }
