e794d92b9da23ee519c239dcb045be31fe3c5920
v-lmb <virginie.amamelia@gmail.com>
2025-10-21 11:20:18 +0000

    feat c program with if, if else, else and random number

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	if else (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
