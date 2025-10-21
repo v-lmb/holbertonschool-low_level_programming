19e77d9fe581d4a519df756655726c14126b0a5d
v-lmb <virginie.amamelia@gmail.com>
2025-10-21 15:48:16 +0000

    fix : adjustment in if and else if conditions

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,17 +13,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	D = n % 10;
-	if (n > D)
+	if (D > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, D);
 	}
-	else if (n < D)
+	else if (D == 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, D);
+		printf("Last digit of %d is %d and is 0\n", n, D);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is 0\n", n, D);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, D);
 	}
 return (0);
 }
