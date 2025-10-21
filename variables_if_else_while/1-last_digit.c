87d3ad8b11baa9e0618182ecbf9d80f6dfe48d2c
v-lmb <virginie.amamelia@gmail.com>
2025-10-21 15:34:32 +0000

    fix : add int D and modulo variable

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,20 +8,22 @@
 int main(void)
 {
 	int n;
+	int D;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 5)
+	D = n % 10;
+	if (n > D)
 	{
-		printf("Last digit of %d and is greater than 5\n", n);
+		printf("Last digit of %d and %d is greater than 5\n", n, D);
 	}
-	else if (n < 6)
+	else if (n < D)
 	{
-		printf("Last digit of %d is negative\n", n);
+		printf("Last digit of %d and %d is less than 6 and not 0\n", n, D);
 	}
 	else
 	{
-		printf("Last digit of %d and is 0\n", n);
+		printf("Last digit of %d and %d is 0\n", n, D);
 	}
 return (0);
 }
