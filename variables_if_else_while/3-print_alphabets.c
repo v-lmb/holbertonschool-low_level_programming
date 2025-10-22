c766b66b9a8f70b85d42ce1aa162abd88a7ca7ca
v-lmb <virginie.amamelia@gmail.com>
2025-10-22 09:04:30 +0000

    feat c program for alphabet upper case and lower case

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a'
+	char d = 'A'
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+		putchar('\n');
+	return (0);
+}
