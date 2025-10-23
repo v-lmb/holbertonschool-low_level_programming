969a2c38874db3c08d4bbe7d1419141cf19833c8
v-lmb <virginie.amamelia@gmail.com>
2025-10-23 18:47:08 +0000

    feat program that prints all the numbers of base 16 in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/** 
+ * main - Entry point 
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+for (num = 0; num <= 15; num++)
+{
+printf("%x", num);
+}
+printf("\n");
+return (0);
+}
