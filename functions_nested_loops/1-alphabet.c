c1792c9c9b9ef7647bd57d8db16352ea5d2cfd00
v-lmb <virginie.amamelia@gmail.com>
2025-10-24 16:48:04 +0000

    feat print alphaber lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+int a = 'a';
+while (a <= 'z')
+	{
+	_putchar(a);
+	a++;
+	}
+_putchar('\n');
+}
