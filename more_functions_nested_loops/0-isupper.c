c7dde21f791a891d6112c98b854691373abfab9b
v-lmb <virginie.amamelia@gmail.com>
2025-10-27 09:40:46 +0000

    feat function check uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * c: int char
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
