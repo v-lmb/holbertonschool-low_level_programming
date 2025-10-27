9d5e25874878c81421c20c30d77bfa0bb133dd50
v-lmb <virginie.amamelia@gmail.com>
2025-10-27 12:54:37 +0000

    feat function draw straight line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ * @n: number of times th character _ should be printed
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+		_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
