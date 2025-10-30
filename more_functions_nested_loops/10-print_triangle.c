7a259b7c4bd56ab451c435801b98257deb3c2eb2
v-lmb <virginie.amamelia@gmail.com>
2025-10-30 21:35:59 +0100

    feat triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - Entry point
+ * @size: size of triangle
+ * Return: Always 0 (Success)
+ */
+void print_triangle(int size)
+{
+	int ligne, espace, hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (ligne = 1; ligne <= size; ligne++)
+	{
+		for (espace = size - ligne; espace > 0; espace--)
+		_putchar(' ');
+
+		for (hash = 0; hash > ligne; hash++)
+		_putchar('#');
+	_putchar('\n');
+	}
+}
