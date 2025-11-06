8ad28151d15a33c037d4fd61399db48e86f14fb8
v-lmb <virginie.amamelia@gmail.com>
2025-11-06 22:24:54 +0100

    feat function that returns the factorial of a given number

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - Entry point
+ * @n: number
+ * Return: actorial of a given number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
