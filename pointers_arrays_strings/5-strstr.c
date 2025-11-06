8c0a902baf06785285c1401d9d91ee4c1a5681c2
v-lmb <virginie.amamelia@gmail.com>
2025-11-06 17:50:49 +0100

    fix if loops

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -8,19 +8,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *h = haystack;
-	char *n = needle;
+	int i, j;
 
-	while (*h != '\0')
+	if (*needle == '\0')
+		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-
-		while (*n != '\0')
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (*h == *n)
-				return (h);
-			h++;
+			if (haystack[i + j] != needle[j])
+			break;
 		}
-		n++;
+		if (needle[j == '\0'])
+			return (haystack + i);
 	}
 	return (NULL);
 }
