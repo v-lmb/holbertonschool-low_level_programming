d3aec101736117796967b161ed73c70bde2be7a9
v-lmb <virginie.amamelia@gmail.com>
2025-11-07 17:50:00 +0100

    feat program that prints its name, followed by a new line

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: character string table
+ * Return: print the name program
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
