ae4098b491832699f1f0bbf303b69e01190c661e
v-lmb <virginie.amamelia@gmail.com>
2025-11-07 17:53:47 +0100

    feat number of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: character string table
+ * Return: the number of arguments passed into it
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
