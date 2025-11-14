dad1984c5f2b515bf1b0e471c84ea1845f360b03
v-lmb <virginie.amamelia@gmail.com>
2025-11-14 17:37:52 +0100

    fix add {} to if

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -9,13 +9,16 @@
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
+	{
 		return;
-
+	}
 		if (d->name)
-		free(d->name);
-
+		{
+			free(d->name);
+		}
 		if (d->owner)
-		free(d->owner);
-
+		{
+			free(d->owner);
+		}
 		free(d);
 }
