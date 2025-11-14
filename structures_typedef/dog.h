74d1301b5bc721baaa367eb434753e879b01655e
v-lmb <virginie.amamelia@gmail.com>
2025-11-14 15:39:38 +0100

    feat new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Dog informations
+ * @name: his name (string)
+ * @age: his age (float)
+ * @owner: his owner (tring)
+ */
+struct dog
+{
+		char *name;
+		float age;
+		char *owner;
+};
+#endif
