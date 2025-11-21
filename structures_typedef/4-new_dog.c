e8d16d74a8ace909d2d762ab8653bb89feb93950
v-lmb <virginie.amamelia@gmail.com>
2025-11-21 11:09:45 +0100

    feat print strings

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strlen - print iformation struct dog
+ * @d: pointer
+ * Return: prints a struct dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
+}
