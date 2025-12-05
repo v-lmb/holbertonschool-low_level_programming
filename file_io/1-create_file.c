bf2166354f52a365689e71b93dbc4334113b467d
v-lmb <virginie.amamelia@gmail.com>
2025-12-05 18:15:14 +0100

    fix third if condition changed == to !=

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
 		{
