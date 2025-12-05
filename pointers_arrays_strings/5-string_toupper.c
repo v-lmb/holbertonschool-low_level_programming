940cb7bc7b8cfcd6221b91797514bc8eec4ca7f0
v-lmb <virginie.amamelia@gmail.com>
2025-12-05 22:53:57 +0100

    feat function that changes all lowercase letters of a string to uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - convert lowercases to uppercase
+ * @s: pointer to string
+ * Return: pointer to the mofided string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - ('a' - 'A');
+		}
+		i++;
+	}
+	return (s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,4 +23,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+char *string_toupper(char *);
 #endif
