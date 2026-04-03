af556a5017117699ef56d3cf8385802b15084941
v-lmb <virginie.amamelia@gmail.com>
2026-04-03 18:21:05 +0200

    Fix: Betty style

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- * _strcmp - Entry point
- * @s1: string1 *
- * @s2: string2 First machin
- * Return: Always 0 (Success)
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: difference between characters
  */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 && (*s1 == *s2))
-{
-    s1++;
-    s2++;
-}
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
 
-return (*s1 - *s2);
+	return (*s1 - *s2);
 }
